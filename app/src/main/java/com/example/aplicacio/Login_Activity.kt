e34c316e25a198bfa526c47bf038872f0890acfc
Marc López Merino <91561153+Jhraxen@users.noreply.github.com>
2022-12-05 19:30:40 +0100

    apk incluido

--- app/src/main/java/com/example/aplicacio/Login_Activity.kt
+++ app/src/main/java/com/example/aplicacio/Login_Activity.kt
@@ -1,4 +1,5 @@
 package com.example.aplicacio
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
@@ -17,17 +18,28 @@ class Login_Activity : AppCompatActivity() {
         SystemClock.sleep(400);
         setTheme(R.style.Theme_Aplicacio);
         setContentView(R.layout.activity_login)
+
+        val sharedPreference =  getSharedPreferences("SharedPrefs", Context.MODE_PRIVATE)
+        val sharedEditor = sharedPreference.edit();
+
+
         val btnSignIn:Button = findViewById(R.id.btnSignIn)
         val txtUsername:EditText = findViewById(R.id.txtUsername)
         val txtPassword:EditText = findViewById(R.id.txtPassword)
         val txtRecordListener: TextView = findViewById(R.id.txtSignUp)
 
-
+        if(sharedPreference.getBoolean("login", false)==true){
+            val intent = Intent(this, Activity_Main::class.java).apply {
+                txtRecordListener.setText("Login correcto")
+            }
+            startActivity(intent)
+        }
 
         btnSignIn.setOnClickListener {
             if (txtUsername.text.toString() == "admin" && txtPassword.text.toString() == "admin"
             ) {
-
+                sharedEditor.putBoolean("login", true);
+                sharedEditor.commit()
                 val intent = Intent(this, Activity_Main::class.java).apply {
                     txtRecordListener.setText("Login correcto")
                 }
